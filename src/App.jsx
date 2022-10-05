23e17ce3acdb4523fe6bd2fbbf7226e18178f5be
William Silva <euwilliamsantana@gmail.com>
2022-10-05 19:43:31 -0300

    Global Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import { Header } from "./components/Header";
+import "./global.css"
 
 export function App() {
 
 
   return (
     <div>
-     <h1>Hello World</h1>
+     <Header/>
     </div>
   )
 }
