551db081c9466c937c902fb3e2fabb6afa3172a9
William Silva <euwilliamsantana@gmail.com>
2022-10-06 08:40:16 -0300

    Estrutura da aplicação: Finalizadno Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from "phosphor-react"
+
 import styles from "./Sidebar.module.css"
 
 export function Sidebar(){
@@ -9,12 +11,18 @@ export function Sidebar(){
       />
 
       <div className={styles.profile}>
+
+        <img 
+          src="https://github.com/wwilliamsantana.png"
+        />
+
         <strong>William Santana</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20}/>
           Editar seu perfil
         </a>
       </footer>
