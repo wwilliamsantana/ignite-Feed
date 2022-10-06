6cf2989502d96a2bdb6029f06fc62dd0de3a501a
William Silva <euwilliamsantana@gmail.com>
2022-10-06 15:42:58 -0300

    Os motores do React - Propriedades do Post + Date-fns

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
@@ -6,7 +7,41 @@ import styles from "./App.module.css"
 import "./global.css"
 
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/wwilliamsantana.png",
+      name: "William Santana",
+      role: "Developer"
+    },
+    content: [
+      {type: "paragraph", content: "Fala galeraa !!"},
+      {type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      {type: "link", content: "jane.design/doctorcare"},
+    ],
+    publishedAt: new Date("2022-09-28 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/jakeliny.png",
+      name: "Jakeliny Oliveira",
+      role: "Educator"
+    },
+    content: [
+      {type: "paragraph", content: "Fala galeraa !!"},
+      {type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      {type: "link", content: "jane.design/doctorcare"},
+    ],
+    publishedAt: new Date("2022-10-02 20:00:00"),
+  },
+]
+
+
 export function App() {
+
+
 
   return (
     <div>
@@ -16,7 +51,16 @@ export function App() {
         <Sidebar/>
 
         <main>
-            <Post/>
+          {
+            posts.map(post => 
+              <Post 
+                key={post.id} 
+                author={post.author} 
+                content={post.content} 
+                publishedAt={post.publishedAt}
+                />
+            )
+          }
         </main>
 
       </div>
