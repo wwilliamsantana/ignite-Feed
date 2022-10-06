4f004badcec01fcb7852fb170cc38d706f0c9be5
William Silva <euwilliamsantana@gmail.com>
2022-10-06 10:51:02 -0300

    Estrutura da aplicação - Estilizações do Comentário

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,7 @@ import styles from "./Comment.module.css"
 
 export function Comment(){
   return (
-    <div className={styles.Comment}> 
+    <div className={styles.comment}> 
 
       <img src="https://github.com/wwilliamsantana.png" />
 
@@ -22,7 +22,7 @@ export function Comment(){
                 </div>
 
                 <button title="Deletar comentário">
-                  <Trash size={20}/>
+                  <Trash size={24}/>
                 </button>
               </header>
 
