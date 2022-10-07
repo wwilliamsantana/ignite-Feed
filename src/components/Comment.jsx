569899705e67130464a14331312fe72771ac44bf
William Silva <euwilliamsantana@gmail.com>
2022-10-07 13:26:43 -0300

    Removendo comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,14 @@ import { ThumbsUp, Trash } from "phosphor-react"
 import { Avatar } from "./Avatar"
 import styles from "./Comment.module.css"
 
-export function Comment(){
+export function Comment({content, onDeleteComment}){
+
+  function handleDeleteComment(){
+    onDeleteComment(content)
+  }
+
+
+
   return (
     <div className={styles.comment}> 
 
@@ -22,12 +29,12 @@ export function Comment(){
                   </time>
                 </div>
 
-                <button title="Deletar comentário">
+                <button onClick={handleDeleteComment} title="Deletar comentário">
                   <Trash size={24}/>
                 </button>
               </header>
 
-              <p>Muito bom Devon, parabéns!!!</p>
+              <p>{content}</p>
 
           </div>
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,18 @@
 import { format, formatDistanceToNow } from "date-fns"
 import { ptBR } from "date-fns/locale"
+import { useState } from "react"
 import { Avatar } from "./Avatar"
 import { Comment } from "./Comment"
 import styles from "./Post.module.css"
 
+
+
 export function Post({author, content, publishedAt}){
+
+  const [comments, setComments] = useState([
+    "Post muito bacana, hein?!"
+  ])
+  const [newCommentText, setNewCommentText] = useState("")
 
   const publishedDateFormat = format(publishedAt, "dd 'de' MMM 'ás' HH:mm'h'", {
     locale: ptBR
@@ -15,7 +23,22 @@ export function Post({author, content, publishedAt}){
     addSuffix: true
   })
 
+  function handleCreateNewComment(){
+    event.preventDefault()
 
+    setComments([...comments, newCommentText ])
+    setNewCommentText("")
+  }
+
+  function handleNewCommentChange(){
+    setNewCommentText(event.target.value)
+  }
+
+  function deleteComment(commentToDelete){
+    const commentsWithoutDeleteOne = comments.filter(comment => comment !== commentToDelete)
+
+    setComments(commentsWithoutDeleteOne)
+  }
 
   return (
     <article className={styles.post}>
@@ -46,20 +69,23 @@ export function Post({author, content, publishedAt}){
        {
         content.map(item => {
           if(item.type === "paragraph"){
-            return <p>{item.content}</p>
+            return <p key={item.content} >{item.content}</p>
           }else if(item.type === "link"){
-            return <p><a href="#">{item.content}</a></p>
+            return <p key={item.content}><a href="#">{item.content}</a></p>
           }
         })
        }
 
       </div>
       
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea
+          name="comment"
           placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -69,9 +95,10 @@ export function Post({author, content, publishedAt}){
       </form>
 
       <div className={styles.commentList}>
-        <Comment/>
-        <Comment/>
-        <Comment/>
+          {
+            comments.map(item => <Comment key={item} content={item} onDeleteComment={deleteComment}/>)
+          }
+
       </div>
 
 
